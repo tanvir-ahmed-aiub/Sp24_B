653c6b32ff8e204b440c5f133bcf57a7fce7f0c6
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-01-30 15:38:10 +0600

    Intro MVC

---
+++ IntroMVC/IntroMVC/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Profile()
+        {
+            var name = "Tanvir";
+            var id = 1;
+            var cgpa = 3.45f;
+
+            
+
+            ViewBag.Name = name;
+            ViewBag.Id = id;
+            ViewBag.Cgpa = cgpa;
+            return View();
+        }
+    }
+}
---
+++ IntroMVC/IntroMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Default
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult SignUp() { 
+            return View();
+        }
+        public ActionResult Submit() {
+            //validation
+            //authentication
+            //if ok
+
+            return Redirect("https://www.aiub.edu");//for external redirection
+            return RedirectToAction("Profile","Dashboard"); //for internal redirection
+            
+        }
+
+    }
+}
