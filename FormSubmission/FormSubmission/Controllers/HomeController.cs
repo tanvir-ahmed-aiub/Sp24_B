44e7b40db4190cac5c0c477121a93f33e14d4053
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-02-06 15:48:43 +0600

    Form Submission

---
+++ FormSubmission/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Index(FormCollection fc) {
+        //    string name = fc["Name"];
+        //    string uname = fc["Uname"];
+        //    return View(fc);
+        //}
+        [HttpPost]
+        public ActionResult Index(string Name, string Uname) { 
+            return View();
+        }
+        public ActionResult Index2() { 
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
