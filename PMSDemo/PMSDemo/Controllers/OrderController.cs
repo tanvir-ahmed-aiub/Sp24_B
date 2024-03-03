3ff0586b7ae86944713a49add1cebcaf875b7def
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-03-03 16:42:15 +0600

    PMS

--- PMSDemo/PMSDemo/Controllers/OrderController.cs
+++ PMSDemo/PMSDemo/Controllers/OrderController.cs
@@ -11,6 +11,8 @@ namespace PMSDemo.Controllers
     public class OrderController : Controller
     {
         // GET: Order
+
+       
         public ActionResult Index()
         {
             var db = new Sp24_b_PMSEntities();
@@ -44,6 +46,30 @@ namespace PMSDemo.Controllers
             }
             var cart = (List<ProductDTO>)Session["cart"];
             return View(cart);
+        }
+        [HttpPost]
+        public ActionResult Place(double Total) { 
+            var order = new Order();
+            order.Amount = Total;
+            order.Status = "Ordered";
+            order.Time = DateTime.Now;
+            order.UserId = 1; //session 
+            var db = new Sp24_b_PMSEntities();
+            db.Orders.Add(order);
+            db.SaveChanges();
+            var cart = (List<ProductDTO>)Session["cart"];
+            foreach (var item in cart) {
+                var pd = new ProductOrder();
+                pd.Price = item.Price;
+                pd.PId = item.Id;
+                pd.Qty = item.Qty;
+                pd.OrderId = order.Id;
+                db.ProductOrders.Add(pd);
+            }
+            db.SaveChanges();
+            Session["cart"] = null;
+            TempData["Msg"] = "Order Placed  with Id no " + order.Id;
+            return RedirectToAction("Index");
         }
     }
 }
